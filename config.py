3dc732f6c126d5a0afa82318bd4f7f6f5ba20e3b
Andrey <mr.karpik.2000@list.ru>
2026-06-02 15:56:16 +0300

    Restore DB_PATH in config with default shop.db

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
 
-
+DB_PATH = os.getenv("DB_PATH", "shop.db")
 
 BACKUP_CHAT_ID = 456884878  # твой Telegram ID
